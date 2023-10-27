49e4dba63b51275321af945259d2aba2d97509c1
Stojanowski, Aleksander <aleksander.stojanowski@intel.com>
2023-10-27 12:09:30 +0200

    Update readme with task definition

--- task/source/main.cpp
+++ task/source/main.cpp
@@ -1,8 +1,9 @@
-#include <iostream>
-
 int main(int argc, char** argv)
 {
-    std::cout << "Hello, world" << std::endl;
+    // TODO: 1. Prepare example data for serialization
+    // TODO: 2. Serialize data to file
+    // TODO: 3. Deserialize data from file
+    // TODO: 4. Verify deserialized data is correct
 
     return 0;
 }
