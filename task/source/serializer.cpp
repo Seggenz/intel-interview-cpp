561914cbb8aab4cb49f7fefd243eb29286a82864
Stojanowski, Aleksander <aleksander.stojanowski@intel.com>
2023-10-27 11:06:04 +0200

    Serializer interface

---
+++ task/source/serializer.cpp
@@ -0,0 +1,14 @@
+#include "serializer.hpp"
+
+namespace Task
+{
+    void SerializeToFile(const DataWrapper* data, const char* filePath)
+    {
+        // TODO: Implement
+    }
+
+    void DeserializeFromFile(DataWrapper*& data, const char* filePath)
+    {
+        // TODO: Implement
+    }
+} // namespace Task
